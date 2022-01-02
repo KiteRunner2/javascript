fe6fc493d3f1323b5000fbd8f429e17af105762c
Przemek Rudzki <przemek@rudzki.ca>
2022-01-02 11:27:10 -0500

    update readme file and isPrime code

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# javascript/typescript
+# Javascript/Typescript code snippets
 
-Code snippets
+
+Content:
+
+<li> isPrime.ts - function to check if number is prime; it will not handle big numbers. For that you need to update it to handle BigInt numbers.
+<li>
--- node/isPrime.ts
+++ node/isPrime.ts
@@ -1,12 +1,13 @@
 function isPrime(num: number): boolean {
   if (num === 1) return false
   if (num === 2) return true
-  for (let i = 2; i < num; i++) {
+  const endIndex = Math.sqrt(num)
+  for (let i = 2; i <= endIndex; i++) {
     if (num % i === 0) return false
   }
   return true
 }
 
-for (let k = 1; k < 500000; k++) {
+for (let k = 1; k < 20; k++) {
   console.log(`${k} is Prime? : ${isPrime(k)}`)
 }
